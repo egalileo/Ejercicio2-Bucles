84b5472aba004de1b61cd72cb1a3106bbb4c3591
Erick Galileo Padilla <erick.galileo@outlook.com>
2022-08-24 08:45:59 -0600

    Ejericio resuelto

--- 02-bucles/src/pkg02/bucles/Bucles.java
+++ 02-bucles/src/pkg02/bucles/Bucles.java
@@ -1,21 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pkg02.bucles;
 
-/**
- *
- * @author PC
- */
-public class Bucles {
+import javax.swing.JOptionPane;
 
-    /**
-     * @param args the command line arguments
-     */
+ /*
+    “Construye un programa que, al recibir como dato un número entero positivo, escriba una figura como la que se muestra a continuación (ejemplo para N = 6):
+
+        1
+        1 2
+        1 2 3
+        1 2 3 4
+        1 2 3 4 5
+        1 2 3 4 5 6
+        1 2 3 4 5
+        1 2 3 4
+        1 2 3
+        1 2
+        1”
+*/
+public class Bucles {
     public static void main(String[] args) {
-        // TODO code application logic here
+        int numero, contador, contador2;
+        
+        do {
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero a evaluar"));
+            if(numero <= 0){
+                JOptionPane.showMessageDialog(null, "Ingrese un numero mayor a cero");
+            }else{
+                //JOptionPane.showMessageDialog(null, "Numero: "+numero+" (Almacenado correctamente)");
+                for(contador=1; contador<=numero; contador++){;
+                    for(contador2=1; contador2<=contador;contador2++){
+                        System.out.print(contador2+" ");
+                    }
+                    System.out.println("");
+                }
+                for(contador=(numero-1); contador >= 1; contador--){
+                    for(contador2=1; contador2<=contador;contador2++){
+                        System.out.print(contador2+" ");
+                    }
+                    System.out.println("");
+                }
+            }
+        }while(numero <= 0);
+        
     }
     
 }
